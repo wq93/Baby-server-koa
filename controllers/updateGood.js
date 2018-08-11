c9c510e1498bfa96fdb8c1be1375ed341bcec715
wq93 <214180117@qq.com>
2018-08-11 14:57:56 +0800

    修改商品接口(不得修改重复商品名)

--- controllers/updateGood.js
+++ controllers/updateGood.js
@@ -3,14 +3,24 @@ const config = require('../config')
 
 module.exports = async (ctx) => {
   const {displayName, type, count, price, source, remark, uuid} = ctx.request.body
-  console.log(displayName, type, count, price, source, remark, uuid)
   if (displayName && type && uuid) {
     try {
-      const findGood = await config.findList(Goods, {uuid})
-      if (findGood.length) {
-        let conditions = {uuid};
+      const list = await config.findList(Goods, {uuid})
+      const listByDisplayName = await config.findList(Goods, {displayName})
+      // 不得重名 且区别不是当前条数据
+      if (listByDisplayName.length > 0 && uuid !== listByDisplayName[0].uuid) {
+        ctx.state = {
+          code: -3,
+          data: {
+            msg: '修改失败,商品名重复'
+          },
+        }
+        return false
+      }
+      if (list.length) {
+        let conditions = {uuid}
         let updates = {$set: {displayName, type, count, price, source, remark}}
-        await Goods.update(conditions, updates)
+        await Goods.update(conditions, updates) // 修改
         ctx.state = {
           code: 0,
           data: {
@@ -21,7 +31,7 @@ module.exports = async (ctx) => {
         ctx.state = {
           code: -5,
           data: {
-            msg: '保存失败',
+            msg: '修改失败',
           }
         }
       }
@@ -29,7 +39,7 @@ module.exports = async (ctx) => {
       ctx.state = {
         code: -1,
         data: {
-          msg: '保存失败'
+          msg: '修改失败'
         },
       }
     }
