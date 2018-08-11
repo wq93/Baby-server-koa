c478738d243087ad9f888dcf0b9f5a45dc6faf76
wq93 <214180117@qq.com>
2018-08-11 13:13:07 +0800

    addGoods接口避免重复添加

--- controllers/addGoods.js
+++ controllers/addGoods.js
@@ -1,16 +1,45 @@
 const {Goods} = require('../db')
 
 module.exports = async (ctx) => {
-
-  console.log('addGoods')
-  console.log(ctx.request.body)
-  ctx.body = {
-    code: -1,
-    message: 'error'
+  const {displayName, type, count, price, source, remark} = ctx.request.body
+  const uuid = `good_${(new Date()).valueOf()}`
+  if (displayName && type) {
+    try {
+      // 避免重复添加
+      const findGood = await Goods.find({displayName})
+      if (findGood.length) {
+        ctx.state = {
+          code: -3,
+          data: {
+            msg: '商品已存在'
+          }
+        }
+        return false
+      } else {
+        // 创建新数据
+        let good = new Goods({displayName, type, count, price, source, remark, uuid})
+        await good.save()
+        ctx.state = {
+          code: 0,
+          data: {
+            good,
+            msg: 'success',
+          }
+        }
+      }
+    } catch (e) {
+      ctx.state = {
+        code: -1,
+        data: {
+          msg: '添加失败'
+        },
+      }
+    }
+  } else {
+    ctx.state = {
+      code: -2,
+      data: {msg: '参数不正确'},
+    }
   }
-  // ctx.state = {
-  //   code: 0,
-  //   msg: 'success',
-  //   data: {}
-  // }
+
 }
--- controllers/goods.js
+++ controllers/goods.js
@@ -1,6 +1,5 @@
 const {Goods} = require('../db')
 module.exports = async (ctx, next) => {
-  console.log('goods')
   let list = await Goods.find()
   ctx.body = {
     code: 0,
