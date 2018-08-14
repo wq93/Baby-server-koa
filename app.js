b06cb263998a3f64b719a76ae19b6234ad730410
wq93 <214180117@qq.com>
2018-08-14 16:12:31 +0800

    'koa-static' 设置图片下载路径

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const Koa = require('koa');
 const app = new Koa();
 const bodyParser = require('koa-bodyparser')
+const serve = require('koa-static');
 // 引入路由分发
 const router = require('./routes/index')
 const config = require('./config')
 const response = require('./middlewares/response')
 
 // 使用响应处理中间件
-app.use(response)
+app.use(serve('public')) //
+  .use(response)
   .use(bodyParser())// 解析请求体
   .use(router.routes()) // 路由分发
   .listen(config.port, () => console.log(`listening on port ${config.port}`))
--- controllers/uploadImgConfig.js
+++ controllers/uploadImgConfig.js
@@ -1,18 +1,19 @@
 const multer = require('koa-multer');//加载koa-multer模块
 //文件上传
 //配置
-var storage = multer.diskStorage({
+const storage = multer.diskStorage({
   //文件保存路径
   destination: function (req, file, cb) {
     cb(null, 'public/images/')
   },
   //修改文件名称
   filename: function (req, file, cb) {
-    var fileFormat = (file.originalname).split(".");
-    cb(null, Date.now() + "." + fileFormat[fileFormat.length - 1]);
+    console.log(file)
+    const fileFormat = (file.originalname).split(".");
+    cb(null, fileFormat[0] + '❤' + Date.now() + "." + fileFormat[fileFormat.length - 1])
   }
 })
 //加载配置
-var upload = multer({storage: storage});
+const upload = multer({storage: storage})
 
 module.exports = upload.single('file')
