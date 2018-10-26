d1c05e9c625a1c9beb7242642469686edff76f9e
wq93 <214180117@qq.com>
2018-10-26 09:51:11 +0800

    编辑宝妈商品接口和删除商品接口

---
+++ models/goods_mother.js
@@ -0,0 +1,15 @@
+// require 默认去node_modules
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var produtSchema = new Schema({
+  'uuid': {type: String}, // id
+  'displayName': String, // 名称
+  'type': String, // 类别 1:日用品 2:洗簌 3:出行
+  'count': Number, // 数量
+  'price': Number, // 价格
+  'source': String, // 来源
+  'remark': String // 备注
+})
+// Good 会自动加s去找数据库的集合
+module.exports = mongoose.model('GoodsMother', produtSchema)
